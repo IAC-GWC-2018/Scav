d940a85865786232e70c91bcd82e14feca625ff9
margro2000 <m.groisman00@gmail.com>
2018-07-27 14:11:38 -0400

    resolve merge conflict

---
+++ Scav/HuntLocationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HuntLocationViewController.swift
+//  Scav
+//
+//  Created by Groisman, Margarita on 7/27/18.
+//  Copyright © 2018 GirlsWhoCode. All rights reserved.
+//
+
+import UIKit
+
+class HuntLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
