e6d18d1d1c3b9117692756f5438d4c4c397a567c
margro2000 <m.groisman00@gmail.com>
2018-08-01 14:12:28 -0400

    linked not working

---
+++ Scav/myLocationsView.swift
@@ -0,0 +1,21 @@
+//
+//  myLocationsView.swift
+//  Scav
+//
+//  Created by Groisman, Margarita on 7/27/18.
+//  Copyright © 2018 GirlsWhoCode. All rights reserved.
+//
+
+import UIKit
+
+class myLocationsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
